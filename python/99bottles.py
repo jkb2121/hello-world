3d00c00cd5db8bad1638b59abf2c49132c2a6ffa
Jkb <jkb@jkbworld.com>
2016-03-22 23:36:17 -0700

    Added Python Directory with the 99bottles.py program :)

---
+++ python/99bottles.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+print "Let's sing a song..."
+
+x = 99
+while True:
+   print "%d bottles of beer on the wall" % (x)
+   x -= 1
+   if (x == 0):
+      break
+
+print "All done!  That was fun!"
+
